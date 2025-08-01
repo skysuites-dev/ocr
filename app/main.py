b35d040376d2b3b1e60139d2b4721f2d27130d77
skysuites-dev <skysuites.co@gmail.com>
2025-08-02 00:12:42 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
+#from fastapi import FastAPI
+#from app.api import endpoints
+#import os
+
+#app = FastAPI(title="OCR Scanner API")
+#app.include_router(endpoints.router)
+
+
 from fastapi import FastAPI
 from app.api import endpoints
 import os
+import asyncio
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
 
 app = FastAPI(title="OCR Scanner API")
 app.include_router(endpoints.router)
+
+if __name__ == "__main__":
+    config = Config()
+    config.bind = [f"[::]:{os.environ.get('PORT', 8000)}"]
+    asyncio.run(serve(app, config))
 
  
