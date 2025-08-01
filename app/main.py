60e94ab18079a67f89b7d7ed1aa7cf085f2d5faf
skysuites-dev <skysuites.co@gmail.com>
2025-08-01 12:17:45 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 from app.api import endpoints
 import os
-import uvicorn
 
 app = FastAPI(title="OCR Scanner API")
 app.include_router(endpoints.router)
 
-# Add this:
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
+ 
